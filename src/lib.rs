f5156ef2970de2dd23292ccd676aee1bbe6df8ee
pscott <scott.piriou@ledger.fr>
2021-01-19 14:34:50 +0100

    Cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,13 @@ use quote::quote;
 #[proc_macro_attribute]
 pub fn test_item(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(item as syn::ItemFn);
-    let name = &input.sig.ident.to_string(); 
+    let name = &input.sig.ident.to_string();
     let func = &input.block;
 
     let r = quote! {
         #[test_case]
         const t: TestType = TestType {
-            modname: module_path!(), 
+            modname: module_path!(),
             name: #name,
             f: || -> Result<(),()> {
                 #func
